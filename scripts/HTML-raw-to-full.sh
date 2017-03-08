8f3f702ed9ef6adc3b85d5112d412d480165a594
tttapa <pieter.pas@outlook.com>
2017-03-09 00:21:05 +0100

    Fixed CRNL problem html merge script

--- scripts/HTML-raw-to-full.sh
+++ scripts/HTML-raw-to-full.sh
@@ -8,15 +8,18 @@ do
 	echo "$i: ${arr[$i]}"
 	if [ $i == 0 ]
 	then
-		arrows="<div class=\"next\"><a href=\"${arr[1]}\"><i class=\"material-icons\">arrow_forward</i></a></div>"
+		arrows="            <div class=\"next\"><a href=\"${arr[1]}\"><i class=\"material-icons\">arrow_forward</i></a></div>"
 	elif [ $i == $((len - 1)) ]
 	then
-		arrows="<div class=\"back\"><a href=\"${arr[(($i-1))]}\"><i class=\"material-icons\">arrow_back</i></a></div>"
+		arrows="            <div class=\"back\"><a href=\"${arr[(($i-1))]}\"><i class=\"material-icons\">arrow_back</i></a></div>"
 	else
-		arrows="<div class=\"back\"><a href=\"${arr[(($i-1))]}\"><i class=\"material-icons\">arrow_back</i></a></div>\
-        <div class=\"next\"><a href=\"${arr[(($i+1))]}\"><i class=\"material-icons\">arrow_forward</i></a></div>"
+		arrows="            <div class=\"back\"><a href=\"${arr[(($i-1))]}\"><i class=\"material-icons\">arrow_back</i></a></div>
+            <div class=\"next\"><a href=\"${arr[(($i+1))]}\"><i class=\"material-icons\">arrow_forward</i></a></div>"
 
 	fi
 	f=${arr[$i]}
-	echo "$(cat ../scripts/header.html)\n$(cat "$f")\n$arrows\n$(cat ../scripts/footer.html)" > "../Full HTML/$f"
+	echo "$(cat ../scripts/header.html)" > "../Full HTML/$f"
+	echo "$(cat "$f")" >> "../Full HTML/$f"
+	echo "$arrows" >> "../Full HTML/$f"
+	echo "$(cat ../scripts/footer.html)" >> "../Full HTML/$f"
 done

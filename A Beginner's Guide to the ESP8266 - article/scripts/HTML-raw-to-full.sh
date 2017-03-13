e9248e4847a773ae832f401e984d61d439d0f925
Pieter Pas <pieter.pas@outlook.com>
2017-03-13 20:12:12 +0100

    Added arrows again

--- A Beginner's Guide to the ESP8266 - article/scripts/HTML-raw-to-full.sh
+++ A Beginner's Guide to the ESP8266 - article/scripts/HTML-raw-to-full.sh
@@ -8,19 +8,31 @@ do
 	echo "$i: ${arr[$i]}"
 	if [ $i == 0 ]
 	then
-		arrows="            <div class=\"next\"><a href=\"${arr[(($i+1))]}\">Next chapter →</a></div>"
+		links="            <div class=\"next\"><a href=\"${arr[(($i+1))]}\">Next chapter →</a></div>"
 	elif [ $i == $((len - 1)) ]
 	then
-		arrows="            <div class=\"back\"><a href=\"${arr[(($i-1))]}\">← Previous chapter</a></div>"
+		links="            <div class=\"back\"><a href=\"${arr[(($i-1))]}\">← Previous chapter</a></div>"
 	else
-		arrows="            <div class=\"back\"><a href=\"${arr[(($i-1))]}\">← Previous chapter</a></div>
+		links="            <div class=\"back\"><a href=\"${arr[(($i-1))]}\">← Previous chapter</a></div>
             <div class=\"next\"><a href=\"${arr[(($i+1))]}\">Next chapter →</a></div>"
-
 	fi
+
+	if [ $i == 0 ]
+	then
+		arrows="            <div class=\"nextArr\"><a href=\"${arr[1]}\"><i class=\"material-icons\">arrow_forward</i></a></div>"
+	elif [ $i == $((len - 1)) ]
+	then
+		arrows="            <div class=\"backArr\"><a href=\"${arr[(($i-1))]}\"><i class=\"material-icons\">arrow_back</i></a></div>"
+	else
+		arrows="            <div class=\"backArr\"><a href=\"${arr[(($i-1))]}\"><i class=\"material-icons\">arrow_back</i></a></div>
+            <div class=\"nextArr\"><a href=\"${arr[(($i+1))]}\"><i class=\"material-icons\">arrow_forward</i></a></div>"
+	fi
+
 	f=${arr[$i]}
 	echo "$(cat ../scripts/header.html)" > "../Full HTML/$f"
 	echo "$(cat "$f")" >> "../Full HTML/$f"
 	echo "<hr>" >> "../Full HTML/$f"
+	echo "$links" >> "../Full HTML/$f"
 	echo "$arrows" >> "../Full HTML/$f"
 	echo "$(cat ../scripts/footer.html)" >> "../Full HTML/$f"
 done

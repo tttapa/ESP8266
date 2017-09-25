2dd511f8eb1de48333390e84b3b85f5360de9f7c
Pieter <pieter.pas@outlook.com>
2017-09-25 12:44:58 +0200

    Update structure temperatureGraph.js and add some comments

--- Examples/16. Data logging/A-Temperature_logger/Uncompressed_data/temperatureGraph.js
+++ Examples/16. Data logging/A-Temperature_logger/Uncompressed_data/temperatureGraph.js
@@ -1,16 +1,69 @@
 var dataArray = [];
 
-var maxDate = new Date();
-var minDate = new Date();
+var defaultZoomTime = 24*60*60*1000; // 1 day
+var minZoom = -6; // 22 minutes 30 seconds
+var maxZoom = 8; // ~ 8.4 months
 
-var zoom = 6;
-var zoomFactor = 24*60*60*1000; // 1 day
-var maxZoom = 14; // ~ 8.4 months
+var zoomLevel = 0;
+var viewportEndTime = new Date();
+var viewportStartTime = new Date();
 
-loadCSV();
+loadCSV(); // Download the CSV data, load Google Charts, parse the data, and draw the chart
+
+
+/*
+Structure:
+
+    loadCSV
+        callback:
+        parseCSV
+        load Google Charts (anonymous)
+            callback:
+            updateViewport
+                displayDate
+                drawChart
+*/
+
+/*
+               |                    CHART                    |
+               |                  VIEW PORT                  |
+invisible      |                   visible                   |      invisible
+---------------|---------------------------------------------|--------------->  time
+       viewportStartTime                              viewportEndTime
+
+               |______________viewportWidthTime______________|
+
+viewportWidthTime = 1 day * 2^zoomLevel = viewportEndTime - viewportStartTime
+*/
+
+function loadCSV() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            dataArray = parseCSV(this.responseText);
+            google.charts.load('current', { 'packages': ['line', 'corechart'] });
+            google.charts.setOnLoadCallback(updateViewport);
+        }
+    };
+    xmlhttp.open("GET", "temp.csv", true);
+    xmlhttp.send();
+    var loadingdiv = document.getElementById("loading");
+    loadingdiv.style.visibility = "visible";
+}
+
+function parseCSV(string) {
+    var array = [];
+    var lines = string.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+        var data = lines[i].split(",", 2);
+        data[0] = new Date(parseInt(data[0]) * 1000);
+        data[1] = parseFloat(data[1]);
+        array.push(data);
+    }
+    return array;
+}
 
 function drawChart() {
-
     var data = new google.visualization.DataTable();
     data.addColumn('datetime', 'UNIX');
     data.addColumn('number', 'Temperature');
@@ -26,8 +79,8 @@ function drawChart() {
 
         hAxis: {
             viewWindow: {
-                min: minDate,
-                max: maxDate
+                min: viewportStartTime,
+                max: viewportEndTime
             },
             gridlines: {
                 count: -1,
@@ -51,86 +104,67 @@ function drawChart() {
     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
     chart.draw(data, options);
+
+    var dateselectdiv = document.getElementById("dateselect");
+    dateselectdiv.style.visibility = "visible";
+
+    var loadingdiv = document.getElementById("loading");
+    loadingdiv.style.visibility = "hidden";
 }
 
-function parseCSV(string) {
-    var array = [];
-    var lines = string.split("\n");
-    for (var i = 0; i < lines.length; i++) {
-        var data = lines[i].split(",");
-        data[0] = new Date(parseInt(data[0]) * 1000);
-        data[1] = parseFloat(data[1]);
-
-        array.push(data);
-    }
-    return array;
-}
-
-function loadCSV() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            dataArray = parseCSV(this.responseText);
-            google.charts.load('current', { 'packages': ['line', 'corechart'] });
-            google.charts.setOnLoadCallback(setRange);
-        }
-    };
-    xmlhttp.open("GET", "temp.csv", true);
-    xmlhttp.send();
-}
-
-function updateDate() {
+function displayDate() { // Display the start and end date on the page
     var dateDiv = document.getElementById("date");
-    var maxDay = maxDate.getDate();
-    var maxMonth = maxDate.getMonth();
-    var minDay = minDate.getDate();
-    var minMonth = minDate.getMonth()
-    if (maxDay == minDay && maxMonth == minMonth) {
-        dateDiv.textContent = (maxDay).toString() + "/" + (maxMonth + 1).toString();
-    } else {
-        dateDiv.textContent = (minDay).toString() + "/" + (minMonth + 1).toString() + " - " + (maxDay).toString() + "/" + (maxMonth + 1).toString();
 
+    var endDay = viewportEndTime.getDate();
+    var endMonth = viewportEndTime.getMonth();
+    var startDay = viewportStartTime.getDate();
+    var startMonth = viewportStartTime.getMonth()
+    if (endDay == startDay && endMonth == startMonth) {
+        dateDiv.textContent = (endDay).toString() + "/" + (endMonth + 1).toString();
+    } else {
+        dateDiv.textContent = (startDay).toString() + "/" + (startMonth + 1).toString() + " - " + (endDay).toString() + "/" + (endMonth + 1).toString();
     }
 }
-
-updateDate();
 
 document.getElementById("prev").onclick = function() {
-    maxDate = new Date(maxDate.getTime() - getZoomTime()/3);
-    setRange();
+    viewportEndTime = new Date(viewportEndTime.getTime() - getViewportWidthTime()/3); // move the viewport to the left for one third of its width (e.g. if the viewport width is 3 days, move one day back in time)
+    updateViewport();
 }
 document.getElementById("next").onclick = function() {
-    maxDate = new Date(maxDate.getTime() + getZoomTime()/3);
-    setRange();
+    viewportEndTime = new Date(viewportEndTime.getTime() + getViewportWidthTime()/3); // move the viewport to the right for one third of its width (e.g. if the viewport width is 3 days, move one day into the future)
+    updateViewport();
 }
 
 document.getElementById("zoomout").onclick = function() {
-    zoom += 1;
-    if(zoom > maxZoom) zoom = maxZoom;
-    else setRange();
+    zoomLevel += 1; // increment the zoom level (zoom out)
+    if(zoomLevel > maxZoom) zoomLevel = maxZoom;
+    else updateViewport();
 }
 document.getElementById("zoomin").onclick = function() {
-    zoom -= 1;
-    if(zoom < 0) zoom = 0;
-    else setRange();
+    zoomLevel -= 1; // decrement the zoom level (zoom in)
+    if(zoomLevel < minZoom) zoomLevel = minZoom;
+    else updateViewport();
 }
 
 document.getElementById("reset").onclick = function() {
-    maxDate = new Date();
-    zoom = 6;
-    setRange();
+    viewportEndTime = new Date(); // the end time of the viewport is the current time
+    zoomLevel = 0; // reset the zoom level to the default (one day)
+    updateViewport();
 }
 document.getElementById("refresh").onclick = function() {
-    maxDate = new Date();
-    loadCSV();
+    viewportEndTime = new Date(); // the end time of the viewport is the current time
+    loadCSV(); // download the latest data and re-draw the chart
 }
 
-function setRange() {
-    minDate = new Date(maxDate.getTime() - getZoomTime());
-    updateDate();
+document.body.onresize = drawChart;
+
+function updateViewport() {
+    viewportStartTime = new Date(viewportEndTime.getTime() - getViewportWidthTime());
+    displayDate();
     drawChart();
 }
-function getZoomTime() {
-    return zoomFactor*(2**(zoom-6));
+function getViewportWidthTime() {
+    return defaultZoomTime*(2**zoomLevel); // exponential relation between zoom level and zoom time span
+                                           // every time you zoom, you double or halve the time scale
 }
-document.body.onresize = drawChart;
+
